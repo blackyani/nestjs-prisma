1afa41fb395a534c87f01501698b60879de13f84
blackyani7777 <blackyani7777@gmail.com>
2022-05-20 14:21:11 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+// swagger
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // !
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Title')
+    .setDescription('Description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
